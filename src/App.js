bc076c91f55839a1eabe2a621a3cfd067f13a8f3
Thiago Silva <thiago.silva@viaconsulting.com.br>
2019-02-06 14:20:04 -0200

    Correção

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import './App.css'
+
+/*
+Importando os componentes
+*/
 import BookShelf from './components/bookshelf/bookShelf'
 import SearchBooks from './components/search/searchBooks'
 import * as BooksAPI from './BooksAPI'
+/*utilizando o dom para rendenizar */
+import { Route } from 'react-router-dom'
+import './App.css'
+
 
 
 
 
 
 export default class App extends React.Component {
-
+/** criando construtor */
   constructor(props){
     super(props)
     this.state = {
@@ -18,29 +24,7 @@ export default class App extends React.Component {
     }
   }
 
-  componentDidMount(){
-    BooksAPI.getAll()
-      .then((books) =>{
-        this.setState(() => ({
-          books
-        }))
-      })
-  }
-
-  MoveBooks = (bookSelect, to) => {
-    if(!this.state.books.find((value) => value.id === bookSelect.id)){
-      bookSelect.shelf = to
-      this.setState((currentState) => ({
-        books : currentState.books.concat(bookSelect)
-      }))
-    }else {
-      this.setState((currentState) => ({
-        books : currentState.books.map(book => book.id === bookSelect.id ? ({...book, shelf: to}): book)
-      }))
-    }
-    BooksAPI.update(bookSelect, to)
-}
-
+/** Metodo render serve para rendenizar as coisas na tela */
   render() {
     return (
       <div>
@@ -53,4 +37,36 @@ export default class App extends React.Component {
       </div>
     )
   }
+
+/** modelo para retornar valores da Api */
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then((books) =>{
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
+ 
+
+
+/** função para mover os livros das estantes */
+  MoveBooks = (bookSelect, stand) => {
+    /** Se o estado do livro for igual ao valor da estante ele exibe*/
+    if(!this.state.books.find((value) => value.id === bookSelect.id)){
+      bookSelect.shelf = stand
+      /** seta o estado atual no array de livros da estante */
+      this.setState((state) => ({
+        books : state.books.concat(bookSelect)
+      }))
+    }else {
+      this.setState((state) => ({
+        books : state.books.map(book => 
+          book.id === bookSelect.id ? ({...book, shelf: stand}): book)
+      }))
+    }
+    /** atualiza os livros */
+    BooksAPI.update(bookSelect, stand)
+}
 }
